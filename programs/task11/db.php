b559dd6918f94162f7d5c7cdcd123415181e3acc
Ayushi <ayushi.desai@matrixhive.com>
2022-04-26 17:33:06 +0530

    using class method

--- programs/task11/db.php
+++ programs/task11/db.php
@@ -12,44 +12,31 @@
 
 
 
-        class config
-        {
-            private $host='localhost';
-            private $username='root1';
-            private $password='root1';
-            private $dbname='time_zone';
-    
-            function __construct()
-            {   
-                if(mysql_connect($this->host,$this->username,$this->password))
-                {
-                    echo "connection successfully";
-                }
-            }
-            function db()
-            {
-                mysql_select_db($this->$dbname);
-            }
-        }
-    
-        $obj=new config();
-        $obj->
+    // class xyz{
+    //   public $db_conn;
+    //   function __construct() {
+    //       $servername='localhost';
+    //       $username='root1';
+    //       $password='root1';
+    //       $dbname = "time_zone";
+          
+    //       $this->db_conn=mysqli_connect($servername,$username,$password,"$dbname");
+  
+    //         if(!$this->db_conn){
+    //             die('Could not Connect MySql Server:' .mysql_error());
+    //           }
+             
+  
+    //       }  
+    //     function get_zone() {
+    //         $arr = array();
+    //       $result = mysqli_query($this->db_conn,"SELECT * FROM `zone` ");
+    //            while($row = mysqli_fetch_array($result)) {
+    //             array_push($arr, $row);
+    //       }
+    //       return $arr;
+    //      }
+    //   }
 
-            $variable = $obj->fetch_data("SELECT * FROM time_zone");
-            for($i=0;$i<count($variable);$i++){  
-                while($row = mysqli_fetch_array($result)) {
-                    ?>
-                    <option value="<?php echo $row['id'];?>"><?php echo $row['name'];?></option>
-                    <?php
-                    }     
-                     $result = $variable[$i]['name'];     
-                     }
-            //insert INTO values in any table
-            $result = $obj->insert_data($name);
-            if($result)
-            echo 'inserted';
-            else{
-            echo 'failed';
-            }
 
 ?>
--- programs/task11/index.php
+++ programs/task11/index.php
@@ -1,3 +1,32 @@
+<?php
+
+class xyz{
+    public $db_conn;
+    function __construct() {
+        $servername='localhost';
+        $username='root1';
+        $password='root1';
+        $dbname = "time_zone";
+        
+        $this->db_conn=mysqli_connect($servername,$username,$password,"$dbname");
+
+          if(!$this->db_conn){
+              die('Could not Connect MySql Server:' .mysql_error());
+            }
+        }  
+
+      function get_zone() {
+          $arr = array();
+         // print_r($row);
+          $result = mysqli_query($this->db_conn,"SELECT * FROM `zone` ");
+                 while($row = mysqli_fetch_array($result)) {
+                      array_push($arr, $row);
+                   print_r($arr);
+        }
+        return $result; 
+       }
+    }
+?>
 <html>
     <head>
          <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.6.0/jquery.min.js"></script>
@@ -35,16 +64,18 @@
         <div class=" container">
             <label for="multiple "> multiple Select:</label>
             <select name="multiple[]" class ="form-control multiple" multiple>
-                <option value="">multiple SelectSelect </option>
+            
+                <!-- <option value="">multiple SelectSelect </option> -->
                 <?php
-                 require_once "db.php";
-                    $result = mysqli_query($conn,"SELECT * FROM `zone` ");
-                    while($row = mysqli_fetch_array($result)) {
+                   $obj = new xyz();
+                   $data = $obj->get_zone();    
+                     foreach($data as $row) { 
                 ?>
+               
                 <option value="<?php echo $row['id'];?>"><?php echo $row['name'];?></option>
                 <?php
-                }
-                ?>
+                   }
+                ?>                                                                                                                  
             <script>
                 $(".multiple").select2({
                   // maximumSelectionLength: 2

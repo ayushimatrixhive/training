9fedd2d4d01238c98454d446407f15ccda60b1c4
Ayushi <ayushi.desai@matrixhive.com>
2022-05-26 12:54:19 +0530

    pagination done

---
+++ programs/task15_main/db.php
@@ -0,0 +1,108 @@
+<?php
+class Makedatatable
+{
+
+    public function __construct()
+    {
+        $this->conn = mysqli_connect("localhost", "root1", "root1", "participant");
+    }
+    public function tableData()
+    {
+       // $limit_per_page = 5;
+
+        $page = "";
+        if (isset($_POST["page_id"])) {
+            $page = $_POST["page_id"];
+        } else {
+            $page = 1;
+        }
+        // $offset = ($page - 1) * $limit_per_page;
+        $query = "SELECT * FROM person_data";
+        if ($_POST['search']) {
+            $search_value = $_POST['search'];
+            $query .= " " . "WHERE" . " " . "fname" . " " . "LIKE" . " " . "'%" . $search_value . "%'" . "OR"
+                                . " " .  "lname" . " " . "LIKE" . " " . "'%" . $search_value . "%'" . "OR" 
+                                . " " .  "dob" . " " . "LIKE" . " " . "'%" .  $search_value. "%'" . "OR" 
+                                . " " .  "age" . " " . "LIKE" . " " . "'%" .  $search_value . "%'" . "OR" 
+                                . " " .  "email" . " " . "LIKE" . " " . "'%" . $search_value . "%'" . "OR" 
+                                . " " .  "phone_no" . " " . "LIKE" . " " . "'%" .  $search_value . "%' " . "OR"
+                                . " " .  "source1" . " " . "LIKE" . " " . "'%" .  $search_value . "%'" . "OR" 
+                                . " " .  "campaign" . " " . "LIKE" . " " . "'%" .  $search_value . "%'" . "OR" 
+                                . " " .  "country" . " " . "LIKE" . " " . "'%" .  $search_value . "%'";
+                            }
+                            
+        // $query = "SELECT * FROM person_data";
+        // if ($_POST['order']) {
+        //     $order_value =$_POST['order'];
+        //     $query.="SELECT * FROM person_data ORDER BY id  ASE";
+        
+
+        $query = "SELECT * FROM person_data ";
+        $main_res = mysqli_query($this->conn, $query);
+        $total_record = mysqli_num_rows($main_res);
+         print_r(" $total_record");
+        $limit = $_POST['limit'] ? $_POST['limit'] : $total_record;
+        $offset = ($page - 1) * $limit;
+
+        //$query .= " " . "LIMIT" . " " . $limit;
+
+        $query .= " " . "LIMIT" . " " . $offset . "," . $limit;
+        echo $query;
+
+        $pag_id = $_POST['page_id'];
+        $offset = ($pag_id - 1) * $limit;
+
+        echo "<br>";
+        $result = mysqli_query($this->conn, $query);
+        $output = "";
+        $total_pages = ceil($result / $limit);
+        if ($result->num_rows > 0) {
+            while ($row = $result->fetch_assoc()) {
+                $output .= '<tr> 
+                <td><input type="checkbox" name="customer_id[]" onclick="userData()" class="delete_customer" value="<?php echo $row["id"]; ?></td>
+                <td>' . $row['id'] . '</td>
+                <td>' . $row['fname'] . '</td>
+                <td>' . $row['lname'] . '</td> 
+                <td>' . $row['dob'] . '</td> 
+                <td>' . $row['age'] . '</td> 
+                <td>' . $row['email'] . '</td> 
+                <td>' . $row['phone_no'] . '</td> 
+                <td>' . $row['source1'] . '</td> 
+                <td>' . $row['campaign'] . '</td> 
+                <td>' . $row['country'] . '</td> 
+                </tr>';
+                
+              
+            }
+            $sql = "select * from person_data";
+            $res = mysqli_query($this->conn, $sql);
+            $total_records = mysqli_num_rows($res);
+            $total_pages = ceil($total_records / $limit);
+            $output .= "<div class = 'pagination' id='pagination' style='margin-left:100%'>";
+
+            for ($i = 1; $i <= $total_pages; $i++) {
+
+                $output .= "<a class='active' id='{$i}' href=''>{$i}</a>";
+            }
+            $output .= "</div>";
+            echo $output;
+        }
+    }
+
+    public function deleteRecord($id)
+    {
+
+        $query = mysqli_query($this->conn, "DELETE FROM person_data WHERE id = '$id'");
+        if ($query == true) {
+            header("Location:view.php");
+        } else {
+            echo "Record does not delete try again";
+        }
+    }
+}
+
+$rowobj = new Makedatatable();
+if (count($_POST) > 0) {
+
+    $rowobj->tableData();
+}
--- programs/task15_main/index.php
+++ programs/task15_main/index.php
@@ -1,158 +1,169 @@
 <?php
-
-
 include 'db.php';
-
-
 if (isset($_GET['id']) && !empty($_GET['id'])) {
     $deleteId = $_GET['id'];
-    $obj->deleteRecord($deleteId);
+    $rowobj->deleteRecord($id);
 }
-
-
-$num_per_page=05;
-
 ?>
-
-
 <!DOCTYPE html>
 <html lang="en">
 
 <head>
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet">
-    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js"></script>
+    <link href="https:cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet">
+    <script src="https:cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js"></script>
 
-    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.6.0/jquery.min.js"></script>
-    <script src="http://ajax.aspnetcdn.com/ajax/jquery.validate/1.11.1/jquery.validate.min.js"></script>
+    <script src="https:ajax.googleapis.com/ajax/libs/jquery/3.6.0/jquery.min.js"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.6.0/jquery.min.js"></script>
 
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>See USERS Details</title>
+    <title>See Employee details</title>
 </head>
 <style>
-    #pagination {
-        margin-left: 700%;
-        display : flex;
-
+    .pagination {
+        margin-left: 50%;
+        padding:20px;
+      
     }
 
-    #pagination a {
+    .pagination a {
         color: black;
+        background-color:green;
+        margin:8px;
         float: left;
         padding: 8px 16px;
         text-decoration: none;
-        transition: background-color .3s;
-       
+        /* transition: background-color .3s; */
     }
 
-    #pagination a.active {
+    .pagination a.active:hover {
         background-color: dodgerblue;
+        padding: 8px 16px;
+        margin:8px;
         color: white;
-     
     }
 
-    #pagination a:hover:not(.active) {
+    .pagination a:hover:not(.active) {
         background-color: #ddd;
-      
     }
+    
 </style>
-
 
 <body>
     <div class="card">
-        <!-- <div class="card-header bg-primary text-white text-center">USER DATA</div> -->
+       
         <div class="card-body " id="data">
             <!-- <div class="search-box" style="width: 25%;margin-left:75%" >
         <input type="text" autocomplete="off" placeholder="Search country..." class="form-control">
         <div class="result"></div> -->
-        <!-- <lable> select data value </lable> -->
 
-            <select id="limit" class="form-select limits" style="width: 10%;" name="limits" onchange="userData()">
-                <option value="">All</option>
+            <select id="limit" class="form-select limits"  onchange="userData()" style="width: 10%;" name="limits">
                 <option value="5">5</option>
                 <option value="10">10</option>
                 <option value="15">15</option>
                 <option value="20">20</option>
                 <option value="25">25</option>
+                <option value="">All</option>
 
             </select>
 
-            <div class="searchbar">
-                <input type="text" placeholder="Search" onkeyup="userData()" class="form-control" id="search" style="width: 30%; margin-left:70%; margin-top: -36px;">
+            <div class="livesearch">
+                <input type="text" placeholder="Search" onkeyup="userData()" class="form-control" id="search" style="width: 30%; margin-left:70%;    margin-top: -36px;
+">
             </div>
             <br>
             <div class="deletebutton">
             <!-- <button type="button" type="submit"  id="deletebtn" value="delete" class="btn btn-primary btn-sm delete"  data-id="<?=$row['id'];?>">Delete</button>  </div> -->
             <button id="delete-btn" name="delete_data" class="btn btn-danger"> Delete </button>
             </div>
+            <br>
 
-            <table class = "table table-bordered">
-                <thead class = "bg-primary text-white text-center"> 
-                     <tr> 
-                        <th>#</th>
+            <table class="table table-bordered">
+                <thead class="bg-primary text-white text-center">
+                    <tr>
+                        <th>#<p><select onclick="userData()" id="ordering">
+                        <option value="ASC">Ascending</option>
+                        <option value="DESC">Descending</option></select></p></th>
                         <th>Id</th>
-                        <th>Fname</th>
-                        <th>Lname</th>
+                        <th>First Name</th>
+                        <th>Last Name</th>
                         <th>DOB</th>
                         <th>Age</th>
                         <th>Email</th>
-                        <th>Phone No</th>
+                        <th>phone no</th>
                         <th>Source1</th>
                         <th>Campign</th>
                         <th>Country</th>
-                        <th>Action</th>
-                     </tr> 
-                    <thead id="review"> 
+                       
+                    </tr>
+                </thead>
+                <tbody id="reviewpage" class="paginate">
+                  <br>
 
+                </tbody>
             </table>
-            <!-- <div id="pagination">
-                <a class="active" id="1" href="">1</a>
-                <a id="2" href="">2</a>
-                <a id="3" href="">3</a>
-            </div> -->
+           
+            
 
             <script>
-
                 function userData(page) {
                     var limit = $("#limit").val();
                     var search = $("#search").val();
-
-                    console.log(limit)
-                    console.log(search)                    
+                    var sorting = $("#ordering").val();
+                    console.log(sorting);
 
                     $.ajax({
                         type: "POST",
                         url: "db.php",
                         data: {
                             limit: limit,
-                            search:search,
-                            page: page
-                         },
-                         success: function(data) {
-                              $("#review").html(data);
-                             }
-                     });
-                 }
-                 userData();
+                            search: search,
+                            data: {
+                                page: page,
+                                sorting:sorting
+                            }
 
-                //  $(document).ready(function() {
-                //     userData();
+                        },
 
-                // });
+                        success: function(data) {
 
-                
-                    $(document).on("click","#pagination a",function(e){
-                        e.preventDefault();
-                        var page_id =$(this).attr("id");
-                        console.log(page_id);
-                        userData(page_id);
-                    })
+                            $("#reviewpage").html(data);
 
 
-                //multiple-delete
-                $("#delete-btn").on("click",function(){
- 
+                        }
+                    });
+                }
+
+                $(document).ready(function() {
+                    userData();    
+
+                });
+
+                $(document).on("click", "#pagination a", function(e) {
+                    e.preventDefault();
+                    var page_id = $(this).attr("id");
+                    var limit = $("#limit").val();
+                    var search = $("#search").val();
+                    
+
+                    $.ajax({
+                        type: "POST",
+                        url: "db.php",
+                        data: {page_id:page_id,
+                            limit:limit,
+                            search:search
+},
+                        success: function (pagedata) {
+                            $(".paginate").html(pagedata);
+                        }
+                    });
+                    //userData(page_id);
+
+                })
+                    //multiple-delete
+                    $("#delete-btn").on("click",function(){
+                    
                     var id=[];
                     // console.log(id);
 
@@ -184,7 +195,6 @@ $num_per_page=05;
                     userData();  
                     };
                     });
-              
             </script>
         </div>
 
